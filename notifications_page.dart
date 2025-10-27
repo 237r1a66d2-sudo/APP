8845c6b15b03aeafd0292a05b2fefd8a438a2b32
237r1a66d2-sudo <237r1a66d2@cmrtc.ac.in>
2025-10-27 14:34:18 +0530

    Create notifications_page.dart

---
+++ notifications_page.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+class NotificationsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(appBar: AppBar(title: Text('Notifications')), body: Center(child: Text('Notifications Page')));
+}
