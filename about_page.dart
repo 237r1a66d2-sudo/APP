f9e84c69b43238f426b33a6e977c13a95df6db32
237r1a66d2-sudo <237r1a66d2@cmrtc.ac.in>
2025-10-27 14:33:33 +0530

    Create about_page.dart

---
+++ about_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(appBar: AppBar(title: Text('About')), body: Center(child: Text('About Page')));
+}
+
+// services_page.dart
+import 'package:flutter/material.dart';
+class ServicesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(appBar: AppBar(title: Text('Services')), body: Center(child: Text('Services Page')));
+}
+
+// contact_page.dart
+import 'package:flutter/material.dart';
+class ContactPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(appBar: AppBar(title: Text('Contact')), body: Center(child: Text('Contact Page')));
+}
